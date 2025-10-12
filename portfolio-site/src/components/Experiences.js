ac3fd912ce788a021d9cecc69c326e4e2adcb5b1
Kritika Parajuli <124526898+kparajul@users.noreply.github.com>
2025-10-12 17:54:22 -0400

    dates in exp

--- portfolio-site/src/components/Experiences.js
+++ portfolio-site/src/components/Experiences.js
@@ -8,7 +8,7 @@ const Experiences = () => (
     <div className='experience-container'>
         <h2>Experiences</h2>
         <div className='experience-box'>
-            <h3>Global EMRA Automation Engineering Intern - NOVELIS</h3>
+            <h3>Global EMRA Automation Engineering Intern - NOVELIS [May 2025 - Present]</h3>
              <div className='text-container'>
                 <li>Built and maintained tools in C# to streamline asset inventory management across automation systems, improving accuracy and visibility for cross-functional teams</li>
                 <li>Applied Agile practices through stakeholder meetings, iterative development, and testing, delivering solutions aligned with user needs and business priorities</li>
@@ -17,7 +17,7 @@ const Experiences = () => (
             </div>
         </div>
         <div className='experience-box'>
-            <h3>Software Engineering Intern - Jobs4Interns</h3>
+            <h3>Software Engineering Intern - Jobs4Interns [May 2024 - August 2024]</h3>
              <div className='text-container'>
                 <li>Developed and optimized data storage and retrieval process using MongoDB, ensuring 30% performance improvement</li>
                 <li>Conducted API testing and reliability analysis, ensuring system stability across environments</li>
--- portfolio-site/src/components/Research.js
+++ portfolio-site/src/components/Research.js
@@ -8,7 +8,7 @@ const Research = () => (
     <div className='research-container'>
         <h2>Research</h2>
         <div className='research-box'>
-            <h3>Quantum Computing - Independent Study</h3>
+            <h3>Quantum Computing - Independent Study [August 2025 - Present]</h3>
              <div className='text-container'>
                 <li>Conduct weekly readings and mathematical analysis from "Quantum Computing Explained" by David McMahon, focusing on the foundations of quantum mechanics and computation theory</li>
                 <li>Solve and verify mathematical problems related to quantum states, operators, and algorithms to deepen conceptual understanding</li>
@@ -16,7 +16,7 @@ const Research = () => (
             </div>
         </div>
         <div className='research-box'>
-            <h3>Requirements Engineering - Research Intern</h3>
+            <h3>Requirements Engineering - Research Intern [May 2024 - August 2024]</h3>
              <div className='text-container'>
                 <li>Conducted in-depth analysis of UML diagrams to evaluate software model quality using quantifiable metrics, following methodology outlined in peer-reviewed research</li>
                 <li>Contributed to an experimental framework aimed at identifying reliable indicators of high-quality software design models</li>
