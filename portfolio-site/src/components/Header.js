f0e1117f755b235acbd892b9744ab3f78a10c26e
Kritika Parajuli <124526898+kparajul@users.noreply.github.com>
2025-10-12 17:51:03 -0400

    added linkedin and updated header

--- portfolio-site/src/components/Header.js
+++ portfolio-site/src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Header.css';
+import { FaLinkedin } from 'react-icons/fa';
 
   const Header = () => (
     <header className="header">
       <div className="image-container">
         <img src={`${process.env.PUBLIC_URL}/myImage.png`} alt="Kritika Parajuli"/>
       </div>
-      <div className='name-container'><h1>Kritika Parajuli</h1></div>
+      <div className='name-container'><h1>
+        Kritika Parajuli {""}
+        <a href="https://www.linkedin.com/in/kritika-parajuli/" target="_blank" rel="noopener noreferrer" className='linkedin-icon'><FaLinkedin size={30} color='#008080' /></a>
+        </h1></div>
       <div className='bio-container'><p>Hi! I am an aspiring software developer with an experience in full-stack development. I'm much more fun and funny in person!</p></div>
     </header>
   );
--- portfolio-site/src/components/Resume.js
+++ portfolio-site/src/components/Resume.js
@@ -7,7 +7,6 @@ const Resume = () => (
       <h3>Contact Me:</h3>
       <p>Email: <a href="mailto:kritikaparajuli55@gmail.com">kritikaparajuli55@gmail.com</a>
       </p>
-      <p>Phone: <a href="tel:+1 4176691036">+1 (417) 669-1036</a></p>
     </div>
     <button className='resume-view' onClick={() => window.open(`${process.env.PUBLIC_URL}/Kritika-Resume.pdf`, '_blank')}>
         View full resume</button>
